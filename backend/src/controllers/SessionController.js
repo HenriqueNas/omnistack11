9d6a8a5ea4d247c3170f138eca11aa4875894469
HenriqueEffecti <henrique.nascimento@effecti.com.br>
2020-03-26 22:10:48 -0300

    back-end general fixes

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -1,31 +1,28 @@
-const conn = require('../database/conn')
+const conn = require("../database/conn");
 
 module.exports = {
-    async create(request, response) {
-        const { id } = request.body;
+  async create(request, response) {
+    const { id } = request.body;
 
-        console.log(id);
-        
-        if (id == null){
-            return response.status(400).json( {
-                error : 'INVALID_SESSION',
-                message : 'Invalid ID'
-            });
-        }
+    if (id == null) {
+      return response.status(400).json({
+        error: "INVALID_SESSION",
+        message: "Invalid ID"
+      });
+    }
 
-        const ong = await conn('ongs')
-                            .where('id', id)
-                            .select('name')
-                            .first();
-        
-        if(!ong) {
-            return response.status(400).json( {
-                error : 'INVALID_SESSION',
-                message : 'No ONG found with this ID.'
-            });    
-        }
-        
-        return response.json(ong);
-    },
-    
-}
+    const ong = await conn("ongs")
+      .where("id", id)
+      .select("name")
+      .first();
+
+    if (!ong) {
+      return response.status(400).json({
+        error: "INVALID_SESSION",
+        message: "No ONG found with this ID."
+      });
+    }
+
+    return response.json(ong);
+  }
+};
